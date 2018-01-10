aa840f9173bcc43d1c02508bc7b9dc480c5e3799
Jeremy Habit <jeremy.habit@gmail.com>
2018-01-10 16:41:23 +0100

    home started

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <Table></Table>
+    <h1>Choisissez</h1>
+    <div class="grid">
+      <div class="cell-2 cell-4-l cell-6-s shaded-box text-center bg-peter-river color-yang " v-for="i in 10">
+        <span class="font-size-jumbo">{{i}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
