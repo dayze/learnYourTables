96d7f8a81486e42857e0fba0bd87ae54c9c4e9dc
Jeremy Habit <jeremy.habit@gmail.com>
2018-01-10 16:50:20 +0100

    home tables display

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1>Choisissez</h1>
-    <div class="grid">
-      <div class="cell-2 cell-4-l cell-6-s shaded-box text-center bg-peter-river color-yang " v-for="i in 10">
+    <div class="grid container justify-center">
+      <div
+        class="cell-2 cell-4-l cell-6-s text-center bg-peter-river color-yang cursor-pointer hover-bg-turquoise transition-2"
+        v-for="i in 10">
         <span class="font-size-jumbo">{{i}}</span>
       </div>
     </div>
