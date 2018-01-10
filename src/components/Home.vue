30c1eee4b78c74e18ec6890f877d1d1d23a9b482
Dez <elye24@gmail.com>
2018-01-10 18:02:26 +0100

    routing for test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,22 +2,21 @@
   <div>
     <h1>Choisissez</h1>
     <div class="grid">
-      <div :class="[index === 0 || index === 5 ? 'offset-1' : '', 'bg-' + color]"
-           class="cell-2 cell-4-l cell-6-s shaded-box text-center color-yang" v-for="(color, index) in listColor">
+      <router-link :key="index" :class="[index === 0 || index === 5 ? 'offset-1' : '', 'bg-' + color]"
+                   class="cell-2 cell-4-l cell-6-s shaded-box text-center color-yang" v-for="(color, index) in listColor"
+                   :to="{ name: 'Test', params: {table: index + 1}}">
         <span class="font-size-jumbo">{{index + 1}}</span>
-      </div>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
-  import Table from './Tables'
+  import Test from './Test'
   import {listColor} from './../staticColor'
   export default {
-    name: 'HelloWorld',
-    components: {
-      Table
-    },
+    name: 'Home',
+    components: {Test},
     computed: {
       listColor () {
         return listColor
