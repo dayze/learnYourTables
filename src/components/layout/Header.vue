c34e5c52df554234fadd0bdba9fcedab0d68ef4c
Jeremy Habit <jeremy.habit@gmail.com>
2018-01-27 12:57:48 +0100

    my statistics button reworked

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,23 +1,32 @@
 <template>
-  <header class="grid slanted-bottom padding-50 text-center">
+  <header>
+    <div class="grid-column-m slanted-bottom padding-50 padding-l text-center justify-center align-items-center-m">
 
-    <!-- TODO : responsive -->
-    <!-- dashboard -->
-    <router-link :to="{ name: 'Dashboard'}" title="See the dashboard"
-                 class="anim-zoom absolute-top-right margin-30 padding rounded-circle bg-yang borders-2 hover-borders-clouds cursor-pointer transition-2">
-      <img class="responsive" src="../../../src/assets/img/stats.png" alt=""/>
-    </router-link>
+      <div class="cell-8 offset-2 cell-6-l offset-3-l offset-0-m">
+        <h1 class="font-size-jumbo size-giant-l size-big-xs margin-0 text-unstyled wide color-yang">
+          <router-link class="inline-block transition-2 APP_font_ananda"
+                       :to="{ name: 'Home'}"
+                       title="Homepage">
+            Learn your tables
+          </router-link>
+        </h1>
+        <span
+          class="APP_font_bebas text-wide size-medium size-normal-s color-peter-river">Tests on multiplication tables</span>
+      </div>
 
-    <div class="cell-12">
-      <h1 class="font-size-jumbo size-giant-m size-big-xs margin-0 text-unstyled wide color-yang">
-        <router-link class="inline-block transition-2 APP_font_ananda"
-                     :to="{ name: 'Home'}"
-                     title="Homepage">
-          Learn your tables
-        </router-link>
-      </h1>
-      <span
-        class="APP_font_bebas text-wide size-medium size-normal-s color-peter-river">Tests on multiplication tables</span>
+
+      <div class="cell-2 cell-3-l">
+        <ul class="list-unstyled text-right padding-right-diffuser">
+          <!-- dashboard -->
+          <li>
+            <router-link :to="{ name: 'Dashboard'}" title="See the dashboard" class="bg-emerald button margin-bottom-m">
+              <img class="responsive margin-5-right" src="../../../src/assets/img/stats.png" alt=""/>
+              <span class="align-middle color-yang">My Statistics</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+
     </div>
   </header>
 </template>
